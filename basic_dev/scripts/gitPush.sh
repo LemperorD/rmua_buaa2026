d9d5c1a54450bc143214e40ec36ad5069db41bb8
LemperorD <ld2382619813@163.com>
2026-05-18 01:26:35 +0800

    添加gitpush脚本

---
+++ basic_dev/scripts/gitPush.sh
@@ -0,0 +1,3 @@
+cd /home/ld/IntelligentUAVChampionshipSimulator/rmua_buaa2026/
+export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
+git push origin RMUA2026
